529f3d6adf3f3cae1ad9f89409cdb70fb2065c1c
David Zhu <david.zhu@research.uwa.edu.au>
2021-08-17 22:38:59 +0800

    Completed quiz game with question data sourced from opentdb.com

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text,question_answer)
     question_bank.append(new_question)
     
 quiz = QuizBrain(question_bank)
+quiz_active = True 
+while quiz_active: #if quiz still has questions remaining 
+    quiz.next_question()
+    quiz_active = quiz.still_has_questions()
 
-# Asking the questions
-quiz.next_question()
-# Checking if answer was correct
-
-# Checking if quiz has ended 
+quiz.quiz_complete()
